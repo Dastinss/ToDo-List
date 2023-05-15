07bdf861b395c9c30be0ef626db3ef28d395f149
Dastinss <sergeyfdaragan@gmail.com>
2023-05-15 23:54:57 +0300

    #13 done up to 01:30:00

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+
+export default {
+    title: 'API'
+}
+
+const settings = { // свойство чтобы цеплялась кука и не было ошибки 401
+    withCredentials: true // withCredentials определяет, должны ли межсайтовые (кроссдоменные) запросы выполняться с использованием учетных данных (cookie)
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then((res) => {
+                setState(res.data);
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
