3979f33ec5f9e7f061f8ee6407d1d366bf06d6d1
Dastinss <sergeyfdaragan@gmail.com>
2023-05-17 00:51:44 +0300

    #13 done up to 02:40:00 (before incapsulation)

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,12 +1,16 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
+import {number} from "prop-types";
 
 export default {
     title: 'API'
 }
 
 const settings = { // свойство чтобы цеплялась кука и не было ошибки 401
-    withCredentials: true // withCredentials определяет, должны ли межсайтовые (кроссдоменные) запросы выполняться с использованием учетных данных (cookie)
+    withCredentials: true, // withCredentials определяет, должны ли межсайтовые (кроссдоменные) запросы выполняться с использованием учетных данных (cookie)
+    headers: {
+        'API-KEY': 'a7ac1bc5-0d23-4742-ab18-200ee19c5490'
+    }
 }
 
 export const GetTodolists = () => {
@@ -24,6 +28,10 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title:'REACT_TEST'}, settings)
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
@@ -31,6 +39,11 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todolistId = '2d645fd4-61c6-4d3b-8a39-e3321239cd0e'
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings)
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
@@ -38,6 +51,11 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todolistId = 'eae4b727-c7a3-4d21-9748-4356eee9fc43'
+        axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, {title: 'SERG THE BEST'}, settings)
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
